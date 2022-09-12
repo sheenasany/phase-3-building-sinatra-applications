3f3dec7c24b319a7d6fc4e5a0764254eb73f1c61
Sheena Sang <sheenasany@gmail.com>
2022-09-11 20:34:37 -0400

    commit tree 04944cdb9c1f0df3536ae34263494bbd73bdec95

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/application_controller.rb
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+        '<h1>And now... the time has come.. 🎤😮🎶 </h1>'
+    end
+  
+  end
